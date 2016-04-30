fda7b766fd3d36d73be18f4fff1e0201aef6cb85
Yoseph Radding <raddingy@lykeme.com>
2016-04-30 19:06:22 -0400

    Debuging info

--- django/contrib/auth/__init__.py
+++ django/contrib/auth/__init__.py
@@ -86,32 +86,39 @@ def authenticate(**credentials):
             credentials=_clean_credentials(credentials))
 
 
-def login(request, user):
+ef login(request, user):
     """
     Persist a user id and a backend in the request. This way a user doesn't
     have to reauthenticate on every request. Note that data set during
     the anonymous session is retained when the user logs in.
     """
+    print("user in login:", user)
     session_auth_hash = ''
     if user is None:
+        print("User is none")
         user = request.user
     if hasattr(user, 'get_session_auth_hash'):
+        print("hash is fucked")
         session_auth_hash = user.get_session_auth_hash()
 
     if SESSION_KEY in request.session:
+        print("ok session is in request.session")
         if _get_user_session_key(request) != user.pk or (
                 session_auth_hash and
                 request.session.get(HASH_SESSION_KEY) != session_auth_hash):
+            print("Flushing session")
             # To avoid reusing another user's session, create a new, empty
             # session if the existing session corresponds to a different
             # authenticated user.
             request.session.flush()
     else:
+        print("Cycling shit")
         request.session.cycle_key()
     request.session[SESSION_KEY] = user._meta.pk.value_to_string(user)
     request.session[BACKEND_SESSION_KEY] = user.backend
     request.session[HASH_SESSION_KEY] = session_auth_hash
     if hasattr(request, 'user'):
+        print("request.user homes")
         request.user = user
     rotate_token(request)
     user_logged_in.send(sender=user.__class__, request=request, user=user)
